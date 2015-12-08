e0666dc513a0e45a5d33b935d65c757e0db3589c
James Thompson <thompsonjamesrichard@gmail.com>
2015-12-08 22:09:43 +0000

    API log in works

--- site/controllers/LoginController.php
+++ site/controllers/LoginController.php
@@ -11,23 +11,22 @@ use yii\authclient\OAuth1;
 
 class LoginController extends Controller
 {
-	private $clientId = '229XBT';
-	private $secret = 'fd590071df7af3a872eec8d61d80f35e';
+	private $clientId = '229XJ3';
+	private $secret = '6545557ab2e86ea84e43c51826b17985';
 	
 	public function actionIndex()
 	{
 		$request = \Yii::$app->request;
 		
-		$clientId = '229XBT';
-		$responseType = 'code';
-		$scope = Html::encode('activity profile');
-		$prompt = 'login';
+		$params = array(
+			"response_type" => 'code',
+			"client_id" => $this->clientId,
+			"scope" => Html::encode('activity profile'),
+		);
 			
 		return $this->redirect(
 				"https://www.fitbit.com/oauth2/authorize?"
-				."response_type=$responseType&"
-				."client_id=$this->clientId&"
-				."scope=$scope"
+				.http_build_query($params)
 		);	
 	}
 	
@@ -36,44 +35,19 @@ class LoginController extends Controller
 		$session = \Yii::$app->session;
 		$request = \Yii::$app->request;
 		
+		$authHeader = 'Authorization: Basic '.base64_encode("$this->clientId:$this->secret");
+		$contentHeader = 'Content-Type: application/x-www-form-urlencoded';
+		$httpHeaders = array($authHeader, $contentHeader);
+		
 		$oauth = new OAuth2();
 		$oauth->clientId = $this->clientId;
-		$oauth->authUrl = 'https://api.fitbit.com/oauth2/token';
-		$oauth->fetchAccessToken($request->get('code'), array(
-			'client_id' => $this->clientId,
-			'grant_type' => 'authorization_code',
-			'redirect_uri' => Yii::getAlias('@web/index.php?r=login/success')."&code=".$request->get('code')
-		));
+		$oauth->clientSecret = $this->secret;
+		$oauth->returnUrl = "http://localhost".Yii::getAlias('@web/index.php/login/success');
+		$oauth->tokenUrl = 'https://api.fitbit.com/oauth2/token';
+		$oauth->fetchAccessToken($request->get('code'), [], $httpHeaders);
+		$token = $oauth->getAccessToken();
 		
-		$oauth->buildAuthUrl(array(
-			'client_id' => $this->clientId,
-			'grant_type' => 'authorization_code',
-			'redirect_uri' => Yii::getAlias('@web/index.php?r=login/success')."&code=".$request->get('code')
-		));
-		
-		$base64auth = base64_encode("229XBT:fd590071df7af3a872eec8d61d80f35et");
-		echo $base64auth;
-		$url = 'https://api.fitbit.com/oauth2/token';
-		$data = array(
-			'client_id' => $this->clientId,
-			'grant_type' => 'authorization_code',
-			'redirect_uri' => Yii::getAlias('@web/index.php?r=login/success')."&code=".$request->get('code')
-		);
-		$options = array(
-				'https' => array(
-						'header'  => "Authorization: Basic ".base64_encode("229XBT:fd590071df7af3a872eec8d61d80f35et").
-									 "Content-type: application/x-www-form-urlencoded",
-						'method'  => 'POST',
-						'content' => http_build_query($data),
-				),
-		);
-		$context  = stream_context_create($options);
-		//$result = file_get_contents($url, false, $context);
-		
-		$session->set('dump', $context);
-		return $this->redirect('@web/index.php?r=site/dump');
-		
-		//$session->set('dump', $request->post());
-		//return $this->redirect('@web/index.php?r=site/dump');
+		$session->set('dump', $token);
+		return $this->redirect('@web/index.php/site/dump');
 	}
 }
